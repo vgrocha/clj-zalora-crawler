9d65d1903b22254d4fbcdba12a7c901f7e2e73a5
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2013-10-30 19:10:53 -0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject zalora-crawler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject zalora-crawler "0.1.0"
+  :description "A small single-threaded scrapper,
+                intended for scrapping zalora.sg home page."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
