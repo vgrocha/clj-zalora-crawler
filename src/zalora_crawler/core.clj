0a9bc83ce1f2032e5d4c71713dedb44d876ae9c9
Vinicius Gravina da Rocha <vinicius.darocha@gmail.com>
2013-10-29 23:20:14 -0200

    working version

--- src/zalora_crawler/core.clj
+++ src/zalora_crawler/core.clj
@@ -41,6 +41,11 @@
       first
       h/text))
 
+(defrecord SKU [page-title brand title price old-price])
+
+(defn make-sku [page-title brand title price old-price]
+  (SKU. page-title brand title price old-price))
+
 (defn parse-item [page-title html-seg]
   (make-sku (text-parse page-title)
         (text-parse (get-first-text-content html-seg [:span.itm-brand]))
